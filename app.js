9ed39ec9e247351a67d330db0cbc9203a4d55207
Gustavo Adolfo Flores González <Gustavo Adolfo Flores Gonzlez>
2016-09-19 19:51:34 -0700

    Se agregaron archivos

--- app.js
+++ app.js
@@ -7,11 +7,8 @@ var app = express();
 // Se declara constante para definir el puerto
 const PORT = 8090;
 
-// Se maneja la primer ruta que es /, la cual equivale a http://localhost:8090/
-app.get('/',function(req,res){
-    // Se manda un texto al navegador
-    res.send('Texto desde el servidor');
-});
+// Se da acceso a los archivos que esten en la carpeta public.
+app.use('/',express.static(__dirname + '/public'));
 
 // Se maneja la primer ruta que es /, la cual equivale a http://localhost:8090/hola/
 app.get ('/hola/', function(req,res){
